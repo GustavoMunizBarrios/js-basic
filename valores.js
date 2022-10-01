535f1a6276ac36e230a3e96538e268e14b10c23c
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-09-30 19:59:20 -0500

    primitive and non-primitive data

---
+++ valores.js
@@ -0,0 +1,15 @@
+// Primitive Data
+
+40 //number
+"Gustavo Muñiz" //string
+
+true //Boolean
+false
+
+null //values placeholder
+undefined 
+
+// Non-Primitive data
+
+[1,2,3] //object
+{nombre: "Gustavo"}

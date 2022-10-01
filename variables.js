934edce306d555f0565132071681870e6838fd8b
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-09-30 20:27:58 -0500

    I add a new example

---
+++ variables.js
@@ -0,0 +1,18 @@
+var name = "Gustavo"; //variable called name
+
+// Declare a variable
+var age;
+
+//initialize a variable
+age = 26;
+age;
+
+//Array
+var elements = ["Computer", "Cellphone"]
+
+//object with complex variable
+var person = { 
+    name:"Gustavo",
+    age: 26
+}
+person;

55f26235ca5036a185c3f81c2919207f2b805dac
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-09-30 21:46:28 -0500

    I add a new example

---
+++ funciones.js
@@ -0,0 +1,13 @@
+//Declarative
+
+function myFunction() { //in this case the function has the name of myFunction
+    return 3;
+}
+
+//Expression (also known as anoymous)
+
+var myFunction = function(a,b) { //in this case the function don't has a name
+    return a + b;
+}
+
+myFunction();

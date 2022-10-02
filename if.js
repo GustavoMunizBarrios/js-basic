7a082d8493047722ece7e8417773c70b6e8d2360
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-10-02 16:28:36 -0500

    I add a new exercise

---
+++ if.js
@@ -0,0 +1,17 @@
+var age = 18;
+
+if (age === 18){
+    console.log("You can vote, this will be your first votation");
+
+} else if (age > 18){
+    console.log("You can vote");
+} else {
+    console.log("You can't vote yet :(")
+
+}
+//------------------------------------------------------------
+
+condition ? true : false;
+
+var number = 1;
+var resultado = number === 1 ? "Yes I'am a one" : "No, I'm not a one"

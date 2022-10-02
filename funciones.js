da0a28b6a3e968bf68e41e049f4215641fd81c04
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-10-01 19:34:07 -0500

    I modified the file and add more comments

--- funciones.js
+++ funciones.js
@@ -1,13 +1,29 @@
-//Declarative
+//Function Declaration
 
 function myFunction() { //in this case the function has the name of myFunction
     return 3;
 }
+//-----------------------------------------------------------------------------------
+function myFunction(a,b) { //in this case the function has the name of myFunction
+    return a + b;
+}
+//-----------------------------------------------------------------------------------
+function saludar(nombre) {
+	console.log(`Hola ${nombre}`);
+}
 
-//Expression (also known as anoymous)
+saludar('Diego');
+//-----------------------------------------------------------------------------------
+
+//Function Expression (also known as anoymous)
 
 var myFunction = function(a,b) { //in this case the function don't has a name
     return a + b;
 }
-
 myFunction();
+//-----------------------------------------------------------------------
+var nombre = function(nombre){
+    console.log(`Hola ${nombre}`)
+}
+
+nombre('Diego');

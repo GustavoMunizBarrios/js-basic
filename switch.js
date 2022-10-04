437bbbf2264fcd4a7c08f92890586dc63c4c0278
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-10-04 17:43:42 -0500

    I add a new exercise

---
+++ switch.js
@@ -0,0 +1,15 @@
+var number = 1;
+
+switch (number) {
+    case 1:
+        console.log("I'm one");
+        break;
+    case 10:
+        console.log("I'm ten");
+        break;
+    case 100:
+        console.log("I'm one hundred");
+        break;
+    default:
+        console.log("I'm nothing");
+}

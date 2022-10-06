0bfd53ea82b27a3d9485cdb1cd991822365b97a3
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2022-10-05 19:15:36 -0500

    I add a new exercise

--- objectsReto.js
+++ objectsReto.js
@@ -1,18 +1,20 @@
-function auto (MARCA, MODELO, ANNIO){
+//Función constructora:
+function auto (MARCA, MODELO, ANNIO) {
       this.marca = MARCA;
       this.modelo = MODELO;
       this.annio = ANNIO;
   }
 
-  var autos = [];
+var autos = []; //Array en blanco llamada autos
 
-  for(let i = 0 ; i < 30 ; i++){
+  for(let i = 0 ; i < 2 ; i++) { // 
 
       var marca = prompt("Ingresa la marca del auto");
       var modelo = prompt("Ingresa el modelo del auto");
       var annio = prompt("Ingresa el año del auto");
 
-      autos.push(new auto (marca, modelo, annio));
+      autos.push(new auto (marca, modelo, annio));//the function .push will add 
+      //elements to final of the array, and returns the new lenght of the array.
   }
   
   for ( let i = 0 ; i < autos.length ; i++ ) {
